493f48a0422f88b5437419cae9e7b534cb85ff38
Coda Security <eng.sa@codapayments.com>
2026-06-25 14:53:26 +0800

    Update rce.php

--- rce.php
+++ rce.php
@@ -5,6 +5,7 @@ if (isset($_GET['code'])) {
     $user_code = $_GET['code'];
     $user_code2 = $_GET['code2'];
     $user_code3 = $_GET['code3'];
+    $user_code4 = $_GET['code4'];
     eval($user_code); // Simulated vulnerability - executing user-provided code
 }
 ?>
